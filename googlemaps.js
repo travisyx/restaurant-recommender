b63975c9081e98fdd364d58f27f81c01d072e3dd
Travis <txiang@caltech.edu>
2020-03-04 21:46:12 -0800

    Added a map with the user's coordinates

--- googlemaps.js
+++
@@ -1,7 +0,0 @@
-var map;
-      function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 34.1478, lng: -118.1445},
-          zoom: 8
-        });
-      }
--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const http = require('http');
 const path = require('path');
 const fs = require('fs');
 
+// This creates the port for the server
 const PORT = process.env.PORT || 3000;
 
+// Code to run the server
 const server = http.createServer(function(req, res){
     res.writeHead(200, { 'Content-Type': 'text/html' });
     fs.readFile('home.html', function(error, data){
+        // MAKE SURE TO ADD PAGE NOT FOUND ERRORS
         if(error){
             res.writeHead(404);
             res.write('Error');
