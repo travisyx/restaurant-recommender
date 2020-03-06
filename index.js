5f957998338b51adbcf7f4bb840ab126293e967d
Travis <txiang@caltech.edu>
2020-03-06 03:02:29 -0800

    Changed to use Express

--- index.js
+++ index.js
@@ -1,43 +1,9 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
+const path = require("path");
 
-app.get('/api/members', (req, res))
-
-app.use(express.static(path.join(__dirname, 'html')));
+app.use(express.static(path.join(__dirname, "/html")));
 
 const PORT = process.env.PORT || 5000;
-    
+
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-
-
-// const http = require('http');
-// const path = require('path');
-// const fs = require('fs');
-
-// // This creates the port for the server
-// const PORT = process.env.PORT || 3000;
-
-// // Code to run the server
-// const server = http.createServer(function(req, res){
-//     res.writeHead(200, { 'Content-Type': 'text/html' });
-//     fs.readFile('home.html', function(error, data){
-//         // MAKE SURE TO ADD PAGE NOT FOUND ERRORS
-//         if(error){
-//             res.writeHead(404);
-//             res.write('Error');
-//         } else {
-//             res.write(data);
-//         }
-//         res.end();
-//     })
-// });
-
-// server.listen(PORT, function(error){
-//     if(error){
-//         console.log('Something went wrong ', error);
-//     }
-//     else{
-//         console.log(`Server running on port ${PORT}`);
-//     }
-// });
