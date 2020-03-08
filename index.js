915d8ba025406f0cd0b6d34766c03f5b48753d40
Travis <txiang@caltech.edu>
2020-03-07 21:35:15 -0800

    Improved the UI

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
+// Makes the html folder static
 app.use(express.static(path.join(__dirname, "/html")));
 app.use(express.json());
 
+// Runs the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
