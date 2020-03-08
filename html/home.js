4e7ce70d82379a91db58831d9845be252e97353b
Travis <txiang@caltech.edu>
2020-03-08 15:46:36 -0700

    Cleaned up code

--- html/home.js
+++ html/home.js
@@ -1,81 +1,109 @@
 var lat = 0;
-var long = 0;
+var lng = 0;
 
 // Asks for permission to get the user's coordinates and stores them if allowed
 function enableLocation() {
+  // Tests if the browser allows for geolocation
   if ("geolocation" in navigator) {
+    // Gets the location and stores it
     navigator.geolocation.getCurrentPosition(async function(position) {
       lat = position.coords.latitude;
-      long = position.coords.longitude;
+      lng = position.coords.longitude;
+
       sessionStorage.setItem("latitude", lat);
-      sessionStorage.setItem("longitude", long);
+      sessionStorage.setItem("longitude", lng);
     });
   } else {
     console.log("Geolocation not available");
   }
 }
+
 // Moves to the map
 function moveToUserCoords() {
+  // Tests if the user enabled geolocation
   if (
-    sessionStorage.getItem("latitude") === null ||
-    sessionStorage.getItem("longitude") === null
+    !(
+      sessionStorage.getItem("latitude") === null ||
+      sessionStorage.getItem("longitude") === null
+    )
   ) {
-    alert(
-      "You have not enabled the location to be set. Please allow for the location to be set."
-    );
-  } else {
-    sessionStorage.setItem("isCustom", "false");
+    sessionStorage.setItem("isCustom", "userCoords");
+
     sessionStorage.setItem(
       "prefUserLoc",
       document.getElementById("prefUserLoc").value
     );
-    window.location.href = "map.html";
+
+    window.location.href = "mapSearch.html";
+
+    // Geolocation not enabled
+  } else {
+    alert(
+      "You have not enabled the location to be set. Please allow for the location to be set."
+    );
   }
 }
+
 // Stores what is inputted for the coordinates and moves to the map
 function moveToCustomCoords() {
   var testLat = document.getElementById("customLat").value;
-  var testLong = document.getElementById("customLong").value;
-  if ((testLat == null || testLat == "", testLong == null || testLong == "")) {
-    alert("At least one of the latitude or longitude inputs is empty.");
-  } else if (
-    testLat < -90 ||
-    testLat > 90 ||
-    testLong < -180 ||
-    testLong > 180
-  ) {
-    alert("Please enter valid coordinates.");
+  var testLng = document.getElementById("customLng").value;
+
+  // Tests if the user did not input anything
+  if (!(testLat == null || testLat == "", testLng == null || testLng == "")) {
+    // Tests if the coordinates are valid
+    if (testLat >= -90 && testLat <= 90 && testLng >= -180 && testLng <= 180) {
+      // Stores the values
+      sessionStorage.setItem(
+        "customLat",
+        document.getElementById("customLat").value
+      );
+
+      sessionStorage.setItem(
+        "customLng",
+        document.getElementById("customLng").value
+      );
+
+      sessionStorage.setItem(
+        "prefCustomLoc",
+        document.getElementById("prefCustomLoc").value
+      );
+
+      sessionStorage.setItem("isCustom", "customCoords");
+      window.location.href = "mapSearch.html";
+
+      // Invalid latitude or longitude
+    } else {
+      alert("At least one of the latitude or longitude inputs is invalid.");
+    }
+
+    // Empty input
   } else {
-    sessionStorage.setItem(
-      "customLat",
-      document.getElementById("customLat").value
-    );
-    sessionStorage.setItem(
-      "customLong",
-      document.getElementById("customLong").value
-    );
-    sessionStorage.setItem(
-      "prefCustomLoc",
-      document.getElementById("prefCustomLoc").value
-    );
-    sessionStorage.setItem("isCustom", "true");
-    window.location.href = "map.html";
+    alert("At least one of the latitude or longitude inputs is empty.");
   }
 }
+
 // Stores what is inputted from the search query and moves to the map
 function moveToCustomSearch() {
   var testSearch = document.getElementById("searchQuery").value;
-  if (testSearch == null || testSearch == "") {
-    alert("The search query is empty.");
-  } else {
+
+  // Tests if the user left the input are empty
+  if (!(testSearch == null || testSearch == "")) {
     sessionStorage.setItem(
       "searchQuery",
       document.getElementById("searchQuery").value
     );
+
     sessionStorage.setItem(
       "prefSearchQuery",
       document.getElementById("prefSearchQuery").value
     );
-    window.location.href = "customMap.html";
+
+    sessionStorage.setItem("isCustom", "searchQuery");
+    window.location.href = "mapSearch.html";
+
+    // Empty input
+  } else {
+    alert("The search query is empty.");
   }
 }
